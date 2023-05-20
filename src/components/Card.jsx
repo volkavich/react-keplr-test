1e32c4c8d824acc9524eb1663246ceb2b821a0bd
Rahman <abdulrahman6093@outlook.com>
2023-05-21 01:30:59 +0530

    changed width

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -29,7 +29,7 @@ let test = (window.load = async () => {
 function Card() {
   return (
     <div className="flex justify-center h-screen items-center">
-      <div className=" bg-green-600/10  shadow-green-700/50 flex backdrop-blur-lg shadow-2xl  flex-col text-center justify-between rounded-lg border border-white/20  h-60 p-6 w-max">
+      <div className=" bg-green-600/10  shadow-green-700/50 flex backdrop-blur-lg shadow-2xl  flex-col text-center justify-between rounded-lg border border-white/20  h-64 pt-6 pb-6 pr-10 pl-10 w-max">
         <h1 className="font-bold text-white text-4xl">Testing Keplr</h1>
         <p className="italic text-white">Testing Keplr With React+Vite</p>
         <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700"></hr>
